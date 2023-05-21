98bbf735f86a4c54fe994d08d7e678611da324c4
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-21 21:24:32 +0800

    Implement Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,47 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import "@fontsource/roboto";
-
-declare module "@mui/material/styles" {
-  interface BreakpointOverrides {
-    xs: false; // removes the `xs` breakpoint
-    sm: false;
-    md: false;
-    lg: false;
-    xl: false;
-    mobile: true; // adds the `mobile` breakpoint
-    tablet: true;
-    laptop: true;
-    desktop: true;
-  }
-}
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#2196f3",
-    },
-    secondary: {
-      main: "#3d5afe",
-    },
-  },
-  typography: {
-    fontFamily: "Roboto",
-  },
-  breakpoints: {
-    values: {
-      mobile: 0,
-      tablet: 640,
-      laptop: 1024,
-      desktop: 1920,
-    },
-  },
-});
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
   </ThemeProvider>
 );
---
+++ src/theme.ts
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material";
+
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: false; // removes the `xs` breakpoint
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true; // adds the `mobile` breakpoint
+    tablet: true;
+    laptop: true;
+    desktop: true;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#2196f3",
+    },
+    secondary: {
+      main: "#3d5afe",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto",
+  },
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1920,
+    },
+  },
+});
+
+export default theme;
